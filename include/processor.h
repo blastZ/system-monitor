f6795f5ca3a7afd1bcd929ea92b364f3e3d613e2
blastz <touchzh1996@outlook.com>
2022-09-27 12:29:23 +0000

    feat(processor): get cpu utilization

--- include/processor.h
+++ include/processor.h
@@ -1,11 +1,14 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+#include <vector>
+
 class Processor {
  public:
   float Utilization();
 
  private:
+  std::vector<int> GetTotalFromStatFile();
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,38 @@
 #include "../include/processor.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+#include <chrono>
+#include <string>
+#include <thread>
+
+#include "../include/format.h"
+
+vector<int> Processor::GetTotalFromStatFile() {
+  vector<string> stat_list = Format::GetLineListFromFile("/proc/stat");
+
+  vector<string> cpu_stat_list =
+      Format::GetLineListFromString(stat_list[0], ' ');
+
+  cpu_stat_list.erase(cpu_stat_list.begin());
+  cpu_stat_list.erase(cpu_stat_list.begin());
+
+  int idle = std::stoi(cpu_stat_list[3]) + std::stoi(cpu_stat_list[4]);
+  int non_idle = std::stoi(cpu_stat_list[0]) + std::stoi(cpu_stat_list[1]) +
+                 std::stoi(cpu_stat_list[2]) + std::stoi(cpu_stat_list[5]) +
+                 std::stoi(cpu_stat_list[6]) + std::stoi(cpu_stat_list[7]);
+  int total = idle + non_idle;
+
+  return vector<int>{total, idle};
+}
+
+float Processor::Utilization() {
+  vector<int> pre = GetTotalFromStatFile();
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+  vector<int> now = GetTotalFromStatFile();
+
+  int totald = now[0] - pre[0];
+  int idled = now[1] - pre[1];
+
+  return (totald - idled) * 1.0f / totald;
+}
